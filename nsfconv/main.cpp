bf6e31a02521279248ec16026133a70c5a8b8557
jsd1982 <james.jdunne@gmail.com>
2024-03-22 19:17:18 -0500

    nsfconv: proper reset between songs

--- nsfconv/main.cpp
+++ nsfconv/main.cpp
@@ -4,6 +4,8 @@
 #include "nsfplay.h"
 
 int main(int argc, char **argv) {
+    xgm::NSFPlayerConfig pc;
+    xgm::NSFPlayer p;
     xgm::NSF nsf;
 
     argc--;
@@ -27,10 +29,20 @@ int main(int argc, char **argv) {
     auto songCount = nsf.GetSongNum();
     printf("%d songs\n", songCount);
 
-    for (int i = 0; i < songCount; i++) {
-        xgm::NSFPlayerConfig pc;
-        xgm::NSFPlayer p;
+    pc.SetValue("PLAY_TIME", (60 * 3 + 30) * 1000);
+    p.SetConfig(&pc);
+    nsf.SetDefaults(pc["PLAY_TIME"], pc["FADE_TIME"], pc["LOOP_NUM"]);
 
+    if (!p.Load(&nsf)) {
+        return 3;
+    }
+
+    p.SetChannels(1);
+
+    xgm::UINT32 samplesLength = 480;
+    auto samples = static_cast<short *>(malloc(sizeof(short) * samplesLength));
+
+    for (int i = 0; i < songCount; i++) {
         std::string wavPath;
         {
             std::ostringstream s;
@@ -48,22 +60,11 @@ int main(int argc, char **argv) {
             return 2;
         }
 
-        pc.SetValue("PLAY_TIME", (60 * 3 + 30) * 1000);
-        p.SetConfig(&pc);
-
-        nsf.SetDefaults(pc["PLAY_TIME"], pc["FADE_TIME"], pc["LOOP_NUM"]);
-        if (!p.Load(&nsf)) {
-            fclose(wav);
-            return 3;
-        }
-
-        p.SetChannels(1);
+        nsf.SetLength(-1);
         p.SetPlayFreq(48000);
         p.SetSong(i);
         p.Reset();
 
-        xgm::UINT32 samplesLength = 480;
-        auto samples = static_cast<short *>(malloc(sizeof(short) * samplesLength));
         memset(samples, 0, samplesLength * sizeof(short));
 
         while (!p.IsStopped()) {
@@ -71,9 +72,10 @@ int main(int argc, char **argv) {
             fwrite(samples, sizeof(short), len, wav);
         }
 
-        free(samples);
         fclose(wav);
     }
+
+    free(samples);
 
     return 0;
 }
